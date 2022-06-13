260fd87ba961363889b7aa99cb5f6de819d8f968
SnO2WMaN <me@sno2wman.net>
2022-06-13 10:20:26 +0900

    may work

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    devshell.url = "github:numtide/devshell";
+
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    devshell,
+    flake-utils,
+    ...
+  } @ inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            devshell.overlay
+          ];
+        };
+      in rec {
+        packages.haskell-mahjong = pkgs.haskellPackages.developPackage {
+          root = ./.;
+        };
+        packages.default = packages.haskell-mahjong;
+
+        devShell = pkgs.devshell.mkShell {
+          imports = [
+            (pkgs.devshell.importTOML ./devshell.toml)
+          ];
+        };
+      }
+    );
+}
